7bdb1da3271b937b4a8c55a1fd969d2db0bab801
Bityacht-Developer <developer@bityacht.io>
2024-02-18 08:36:30 +0800

    project initialize
    project initialize

---
+++ go_movie_back_end/cmd/api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+const port=8080
+
+type application struct {
+	Domain string
+
+}
+
+//
+//func (app application) handlerFunc(w http.ResponseWriter, r *http.Request) {
+  //  fmt.Fprintf(w, "Hello, %s!", app.Domain)
+//}
+//
+func main(){
+	//1.設置應用程式config/setting up application config
+var app application//從application struc中提取資訊並做application設置
+	//2.read from cmd line
+
+
+//3.connect to a db
+app.Domain = "example.com"
+
+//3.1Define routes and handlers
+//http.HandleFunc("/", app.handlerFunc)
+
+//4.start a server
+err :=http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+if err!=nil{
+	log.Fatal(err)
+}
+}
---
+++ go_movie_front_end/src/components/EditMovie.js
@@ -0,0 +1,12 @@
+
+const EditMovie = () => {
+
+    return(
+        <div>
+            <h2>Edit Movie</h2>
+            <hr />
+        </div>
+    )
+}
+
+export default EditMovie;
