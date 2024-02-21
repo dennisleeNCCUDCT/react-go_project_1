ffac80e2c39336194e8dede3d79ba9d22d7fd447
Bityacht-Developer <110462011@g.nccu.edu.tw>
2024-02-22 05:46:46 +0800

    feat:update Movies.js
    update movies' content from front-end data to backend-end data

--- go_movie_front_end/src/components/Movies.js
+++ go_movie_front_end/src/components/Movies.js
@@ -2,59 +2,51 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Movies = () => {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect( () => {
-        let moviesList = [
-            {
-                id: 1,
-                title: "Highlander",
-                release_date: "1986-03-07",
-                runtime: 116,
-                mpaa_rating: "R",
-                description: "Some long description",
-            },
-            {
-                id: 2,
-                title: "Raiders of the Lost Ark",
-                release_date: "1981-06-12",
-                runtime: 115,
-                mpaa_rating: "PG-13",
-                description: "Some long description",
-            },
-        ];
+  useEffect(() => {
+    const headers = new Headers();
+    headers.append("Content-type", "application/json"); //insert headers to innitial api request
+    const requestOptions = {
+      method: "Get", //remember to add "," after every parameter
+      headers: headers, //remember to add "," after every parameter
+    };
+    fetch(`http://localhost:8080/movies`, requestOptions) //fetch api請求，使用requestOptions參數
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-        setMovies(moviesList)
-    }, []);
-
-    return(
-        <div>
-            <h2>Movies</h2>
-            <hr />
-            <table className="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Movie</th>
-                        <th>Release Date</th>
-                        <th>Rating</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {movies.map((m) => (
-                        <tr key={m.id}>
-                            <td>
-                                <Link to={`/movies/${m.id}`}>
-                                    {m.title}
-                                </Link>
-                            </td>
-                            <td>{m.release_date}</td>
-                            <td>{m.mpaa_rating}</td>
-                        </tr>    
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Movies</h2>
+      <hr />
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Movie</th>
+            <th>Release Date</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((m) => (
+            <tr key={m.id}>
+              <td>
+                <Link to={`/movies/${m.id}`}>{m.title}</Link>
+              </td>
+              <td>{m.release_date}</td>
+              <td>{m.mpaa_rating}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Movies;
