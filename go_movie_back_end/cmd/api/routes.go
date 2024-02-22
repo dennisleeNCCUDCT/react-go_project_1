39e3290dc0864b171203084ab7bb5020480ba61f
Bityacht-Developer <developer@bityacht.io>
2024-02-22 10:30:47 +0800

    fix:handler.go/routes.go problems
    1.fix package name from "modles"->"models"(in Movies.go)
    
    2.fix route problem in routes.go by changing r * from http.response to http.request
    
    3.change sequence of rd(index:0) and rd(index:1)
    
    4.add .yaml(github depenabot require

--- go_movie_back_end/cmd/api/routes.go
+++ go_movie_back_end/cmd/api/routes.go
@@ -7,11 +7,11 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func (app *application) routes() http.Handler{//app *application->Any variable called app can use this function(routes),and //http.Handler->means this is a http.Handler type function
+func (app *application) routes() http.Handler { //app *application->Any variable called app can use this function(routes),and //http.Handler->means this is a http.Handler type function
 	//create a route mux
-	mux:=chi.NewRouter()
-	mux.Use(middleware.Recoverer)//Recoverer is a middleware that recovers from panics, logs the panic (and a backtrace), and returns a HTTP 500 (Internal Server Error) status if possible. Recoverer prints a request ID if one is provided.
-	mux.Use(app.enableCORS)//no "()"
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer) //Recoverer is a middleware that recovers from panics, logs the panic (and a backtrace), and returns a HTTP 500 (Internal Server Error) status if possible. Recoverer prints a request ID if one is provided.
+	mux.Use(app.enableCORS)       //no "()"
 	//
 	mux.Get("/", app.Home)
 	mux.Get("/movies", app.ALLMovies)
--- go_movie_back_end/internal/models/Movie.go
+++ go_movie_back_end/internal/models/Movie.go
@@ -1,15 +1,16 @@
-package modles
+package models
 
 import "time"
+
 type Movie struct {
-ID int `json:"id"`
-Title string `json:"title"`
-ReleaseDate time.Time `json:"release_date"`
-RunTime int `json:"runtime"`
-MPAARating string `json:"mpaa_rating"`
-Description string `json:"description"`
-Image string `json:"image"`
-CreatedAt time.Time `json:"-"`
-UpdatedField time.Time `json:"-"`//ignore this using"-""
+	ID           int       `json:"id"`
+	Title        string    `json:"title"`
+	ReleaseDate  time.Time `json:"release_date"`
+	RunTime      int       `json:"runtime"`
+	MPAARating   string    `json:"mpaa_rating"`
+	Description  string    `json:"description"`
+	Image        string    `json:"image"`
+	CreatedAt    time.Time `json:"-"`
+	UpdatedField time.Time `json:"-"` //ignore this using"-""
 
 }
