e3e83c65f6977d6abaecae84b2c5fe4b96e7cc77
Bityacht-Developer <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2024-03-07 10:31:05 +0800

    fix:Authenticator func
    fix Authenticator->Auth(in main.go)

--- go_movie_back_end/cmd/api/db.go
+++ go_movie_back_end/cmd/api/db.go
@@ -9,33 +9,28 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib" //go get github.com/jackc/pgx/v4/stdlib
 )
 
-func openDB(dsn string) (*sql.DB, error){
-	db, err :=sql.Open("pgx", dsn)
-	if err != nil{
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
 		return nil, err
 	}
-	err=db.Ping()
-	if err != nil{
+	err = db.Ping()
+	if err != nil {
 		return nil, err
 	}
 
-	return db ,nil
+	return db, nil
 }
 
-func (app *application) connectDB() (*sql.DB,error){
-connection, err :=openDB(app.DSN)
-if err!=nil{
-	return nil, err
+func (app *application) connectDB() (*sql.DB, error) {
+	connection, err := openDB(app.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Connect to Postgres")
+	return connection, nil
+
 }
 
-log.Println("Connect to Postgres")
-return connection, nil
-
-}
 //chatgpt instructions
-type Authenticator struct {
-    // Add fields related to authentication, such as secret keys, token expiration, etc.
-    SecretKey string
-    // Other fields...
-}
-
--- go_movie_back_end/cmd/api/main.go
+++ go_movie_back_end/cmd/api/main.go
@@ -9,44 +9,40 @@ import (
 	"net/http"
 )
 
-
-const port=8080
+const port = 8080
 
 type application struct {
 	Domain string
-DSN string
-DB repository.DatabaseRepo
-auth *Authenticator
-} 
+	DSN    string
+	DB     repository.DatabaseRepo
+	auth   *Auth
+}
 
-//
-//func (app application) handlerFunc(w http.ResponseWriter, r *http.Request) {
-  //  fmt.Fprintf(w, "Hello, %s!", app.Domain)
-//}
-//
-func main(){
+//	func (app application) handlerFunc(w http.ResponseWriter, r *http.Request) {
+//	 fmt.Fprintf(w, "Hello, %s!", app.Domain)
+//	}
+func main() {
 	//1.設置應用程式config/setting up application config
-var app application//從application struc中提取資訊並做application設置
+	var app application //從application struc中提取資訊並做application設置
 
 	//2.read from cmd line
-flag.StringVar(&app.DSN,"dsn","host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5","Postgres connect string")//for connection of postgres DB
-flag.Parse()
-//3.connect to a db
-conn, err:=app.connectDB()
-if err!=nil {
-	log.Fatal(err)
-}
-app.DB= &dbrepo.PostgresDBRepo{DB: conn}
-defer app.DB.Connection().Close()
-//
-app.Domain = "example.com"
-log.Println("starting application on port",port)
-//3.1Define routes and handlers
-//no need after adding app.routes function , http.HandleFunc("/", Hello)//using http start handle function->Hello function in handler.go on http://8080"/""
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connect string") //for connection of postgres DB
+	flag.Parse()
+	// 3.connect to a db
+	conn, err := app.connectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
+	app.Domain = "example.com"
+	log.Println("starting application on port", port)
+	//3.1Define routes and handlers
+	//no need after adding app.routes function , http.HandleFunc("/", Hello)//using http start handle function->Hello function in handler.go on http://8080"/""
 
-//4.starting a server
-err =http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
-if err!=nil{
-	log.Fatal(err)
-}
+	// 4.starting a server
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	if err != nil {
+		log.Fatal(err)
+	}
 }
